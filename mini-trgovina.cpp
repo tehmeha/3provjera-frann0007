9c6f165b3692b19b46827351cbd860651091cd19
TommyLeeJones <antezelenika12@gmail.com>
2017-05-31 12:09:14 +0200

    mjenjanje podataka prema barkodu

--- mini-trgovina.cpp
+++ mini-trgovina.cpp
@@ -95,6 +95,17 @@ int main()
         }
         else if( izbor == 5 )
         {
+            unsigned long long int barkod_pretraga;
+            cout << "Upisite barkod po kojem zelite naci artikl: ";
+            cin >> barkod_pretraga;
+            bool pronadjen = false;
+            for (int i = 0; i < brojArtikla; i ++ )
+            {
+                if(barkod_pretraga==barkod[i])
+                    cout <<"Unesite novu cijenu: " << endl;
+                cin >> cijena[i];
+                break;
+            }
 
         }
         else if( izbor == 6 )
